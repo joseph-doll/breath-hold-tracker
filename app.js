5bb1d373150598f0f028c77946030049c81b7cc2
Joseph Doll III <jdoll003@gmail.com>
2021-12-29 13:01:19 -0800

    add bootstrap validations and express error handling

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const methodOverride = require('method-override');
 const BreathHold = require('./models/breathHold');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://127.0.0.1:27017/breath-hold-tracker', {
     useNewUrlParser: true,
@@ -23,8 +25,10 @@ const app = express();
 
 app.engine('ejs', ejsMate); //use ejs-locals for all ejs templates
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
@@ -42,36 +46,46 @@ app.get('/breathholds/new', (req, res) => {
     res.render('breathholds/new');
 });
 
-app.post('/breathholds', async (req, res, next) => {
+app.post('/breathholds', catchAsync(async (req, res, next) => {
     const breathhold = new BreathHold(req.body.breathhold);
     await breathhold.save();
     res.redirect(`breathholds/${breathhold._id}`);
-});
+}));
 
-app.get('/breathholds/:id', async (req, res) => {
+app.get('/breathholds/:id', catchAsync(async (req, res) => {
     const breathhold = await BreathHold.findById(req.params.id);
     res.render('breathholds/show', { breathhold });
-});
+}));
 
-app.get('/breathholds/:id/edit', async (req, res) => {
+app.get('/breathholds/:id/edit', catchAsync(async (req, res) => {
     const breathhold = await BreathHold.findById(req.params.id);
     res.render('breathholds/edit', { breathhold });
-});
+}));
 
-app.put('/breathholds/:id', async (req, res) => {
+app.put('/breathholds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const breathhold = await BreathHold.findByIdAndUpdate(id, {
         ...req.body.breathhold,
     });
     await breathhold.save();
     res.redirect(`/breathholds/${breathhold._id}`);
-});
+}));
 
-app.delete('/breathholds/:id', async (req, res) => {
+app.delete('/breathholds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await BreathHold.findByIdAndDelete(id);
     res.redirect('/breathholds');
-})
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    if (!err.message) err.message = 'Oh no, something went wrong';
+    res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
     console.log('Serving on localhost:3000');
--- models/breathHold.js
+++ models/breathHold.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BreathHoldSchema = new Schema({
-    name: String,
-    duration: Number,
-    type: String,
-    orifice: String,
-    position: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    orifice: {
+        type: String,
+        required: true,
+    },
+    position: {
+        type: String,
+        required: true,
+    },
     underInfluence: {
         type: String, 
         default: 'no',
@@ -15,7 +30,7 @@ const BreathHoldSchema = new Schema({
         type: Date, 
         default: Date.now,
         required: true,
-    }
+    },
 });
 
 module.exports = mongoose.model('Breathhold', BreathHoldSchema);
