e2d6b40b5159f1ea1531b6e44a7952e21fc94401
Joseph Doll III <jdoll003@gmail.com>
2021-12-30 17:31:59 -0800

    add start script

--- app.js
+++ app.js
@@ -141,6 +141,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
+const port = process.env.PORT || 3000
 app.listen(3000, () => {
-    console.log('Serving on localhost:3000');
+    console.log(`Serving on port ${port}`);
 });
